5b8de57953b650cb8e392913b88a4ec8b25f9ac1
sbmsc <chauhanshubham089@gmail.com>
2019-06-11 21:48:16 +0530

    x

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ request({ url, json: true }, (error, response) => {
     }% chance of rain.`
   );
 });
+
+request(
+  {
+    url:
+      "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2Jtc2MiLCJhIjoiY2p3cm53OG85MGIweDN5cDJpNGFwMXdidyJ9.hOfHzfC291dIHItkAA50EA&limit=1",
+    json: true
+  },
+  (error, response) => {
+    console.log(
+      `The longitude of ${response.body.features[0].place_name} is ${
+        response.body.features[0].center[0]
+      }  and the latitude is ${response.body.features[0].center[1]}`
+    );
+  }
+);
