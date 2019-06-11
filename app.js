1d23671040bcb568553a622808f41a4b4497da19
sbmsc <chauhanshubham089@gmail.com>
2019-06-11 15:54:53 +0530

    customizing-httprequests

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/22217fa5c78cce2357de4ac34de63e45/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/22217fa5c78cce2357de4ac34de63e45/37.8267,-122.4233?units=si";
 
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+request({ url, json: true }, (error, response) => {
+  console.log(
+    `${response.body.daily.summary}. It is currently ${
+      response.body.currently.temperature
+    } degrees out. There is a ${
+      response.body.currently.precipProbability
+    }% chance of rain.`
+  );
 });
